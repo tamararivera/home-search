bb561176d1a8ebd78b4d6b27e13a1f6ad2a216fe
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-06-13 01:06:40 -0700

    Sending the data back to the client.

--- routes/search.js
+++ routes/search.js
@@ -9,77 +9,70 @@ var parser = new xml2js.Parser({explicitArray: false, explicitRoot: false});
 var yelpToken;
 
 router.post('/', function(req, res, next) {
-  var location = req.body.location;
-  var yelpResults;
-  //I have to call the accessToken function only when necessary
-  yelp.accessToken(process.env.YELP_ID, process.env.YELP_SECRET)
-    .then(response => {
-    const client = yelp.client(response.jsonBody.access_token);
+    var location = req.body.location;
+    var yelpResults;
+    //I have to call the accessToken function only when necessary
+    yelp.accessToken(process.env.YELP_ID, process.env.YELP_SECRET)
+        .then(response => {
+            const client = yelp.client(response.jsonBody.access_token);
 
-    client.search({
-      location: location,
-      categories: 'apartments,condominiums'
-    }).then(response => {
-      yelpResults = response.jsonBody.businesses;
-      
-      var promise = new Promise( function(resolve, reject) {
-       resolve(yelpResults.filter(getDataFromZillow));
-      });
-      return promise;
-    }).then(response => {
-      console.log(response);
-      res.send(response);
-  });
-  }).catch(e => {
-    console.log(e);
-  });
+            client.search({
+                location: location,
+                categories: 'apartments,condominiums'
+            }).then(response => {
+
+                yelpResults = response.jsonBody.businesses;
+
+                let map = yelpResults.map(result => {
+                    return getDataFromZillow(result);
+                });
+
+                return Promise.all(map).then(function (result) {
+                    res.send(result.filter(function(x) {return x.unitsAvailable}));
+                });
+
+            });
+        }).catch(e => {
+        console.log(e);
+    });
 });
 
 function shouldRetry(error, response, body) {
+    var retry = false;
     parser.parseString(body, function(err, result) {
-      if (err) {
-        return true;
-      }
-      else{
-        return false;
-      }
+        retry = !!err;
     });
-  }
+    return retry;
+}
 
-function getDataFromZillow(element, index, array) {
-  
-  request.get({
-      url: 'http://www.zillow.com/webservice/GetSearchResults.htm',
-      qs: {
-        'zws-id': process.env.ZILLOW_KEY,
-        address: element.location.address1,
-        citystatezip: element.location.zip_code
-      },
-      retryStrategy: shouldRetry
-    }, function (error, zillowResponse, body) {
-        if(error){
-          return false;
-        }
-        
+function getDataFromZillow(element) {
+
+    return request.get({
+        url: 'http://www.zillow.com/webservice/GetSearchResults.htm',
+        qs: {
+            'zws-id': process.env.ZILLOW_KEY,
+            address: element.location.address1,
+            citystatezip: element.location.zip_code
+        },
+        retryStrategy: shouldRetry
+    }).then( function (response) {
+
+        let body = response.body;
         parser.parseString(body, function (err, result) {
-          if(err){
-            return false;
-          }
-
-          if(!result.response){
-            return false;
-          }
-          if(!result.response.results.result.length){
-            element.unitsAvailable = 1;
-          }
-          else{
-            element.unitsAvailable = result.response.results.result.length;
-          }
-          return true;
+            if(err || !result.response){
+                element.rejected = true;
+            } else {
+                if(!result.response.results.result.length){
+                    element.unitsAvailable = 1;
+                }
+                else{
+                    element.unitsAvailable = result.response.results.result.length;
+                }
+            }
         });
-
+        return element;
     });
-  
+
 }
 
 module.exports = router;
