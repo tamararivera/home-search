5503e8845bce02ab32616743e86e8aedf10db3b3
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-06-14 01:36:16 -0700

    Added a pretty version for the markers.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -51,8 +51,20 @@ function populateMap(data) {
   data.forEach(function (element, index, array) {
     var marker = new google.maps.Marker({
       position: { lat: element.coordinates.latitude, lng: element.coordinates.longitude }});
-
     marker.setMap(map);
+
+    var markerInfo = '<h4>'+ element.name +'</h4><div><img class="yelp-rating" src="/images/yelp/'+ element.rating
+      +'.png"><a target="_blank" href="'+ element.url +'"><img class="yelp-logo" src="/images/yelp/logo.png"></a><p>Based on '
+      + element.review_count + ' reviews</p><a href="' + element.zillowLink + '">'
+      + element.unitsAvailable + ' available units on Zillow</a></div>';
+    var infowindow = new google.maps.InfoWindow({
+      content: markerInfo,
+      maxWidth: 200
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
   })
 
 }
