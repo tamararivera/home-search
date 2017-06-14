063a87bbe2a7ed8143159f8711edaea3446f7cce
Tamara Rivera Farias <tamita.rivera@gmail.com>
2017-06-13 20:55:04 -0700

    Added the map and the markers.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,5 @@
 /* globals $*/
+var map;
 $('#searchButton').on('click', function (event) {
   var formLocation = $('#searchText').val();
   var results = (localStorage['HomeSearchTestResults'])? JSON.parse(localStorage['HomeSearchTestResults']) : [];
@@ -10,16 +11,48 @@ $('#searchButton').on('click', function (event) {
           location: formLocation
         },
         success: function (data) {
+          populateMap(data);
           results = data;
           localStorage['HomeSearchTestResults'] = JSON.stringify(results);
         },
         error: logAjaxError
       });
     }
+  } else {
+    populateMap(results);
   }
   event.preventDefault();
 });
 
 function logAjaxError(jqXHR, textStatus, errorThrown) {
   console.log('AJAX error. Status:', textStatus, 'error:', errorThrown);
+}
+
+function setCenter(data) {
+  var lat = 0;
+  var lng = 0;
+  data.forEach(function (element, index, array) {
+    lat += element.coordinates.latitude;
+    lng += element.coordinates.longitude;
+  });
+
+  lat = lat / data.length;
+  lng = lng / data.length;
+  map.setCenter( { lat: lat, lng: lng });
+}
+
+function populateMap(data) {
+  if (!map) {
+    map = new google.maps.Map( $('#map')[0] );
+    map.setZoom( 16 );
+  }
+
+  setCenter(data);
+  data.forEach(function (element, index, array) {
+    var marker = new google.maps.Marker({
+      position: { lat: element.coordinates.latitude, lng: element.coordinates.longitude }});
+
+    marker.setMap(map);
+  })
+
 }
